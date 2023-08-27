ebdef660c416814c948387b0e9066fff132ce06f
Vilson Apostolovski II <vilsonapo@gmail.com>
2023-08-27 14:51:22 -0400

    Added basic routes for serving html static files

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
 
+// Access 'public' directory files
+app.use(express.static('public'));
+
+// Route to serve index.html file
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+// Route to serve notes.html file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
